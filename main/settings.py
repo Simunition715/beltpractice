202e633df1fa11f5e99c96b2c15ce54af9191811
William Blackwell <williamblackwell@Williams-MacBook-Air.local>
2017-02-22 18:19:59 -0600

    new

--- main/settings.py
+++ main/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
-    '/djregistration/apps/main/static/css',
+    '/djregistration/apps/main/static/',
 ]
