a1913fb6b59add70565b435dc4e575543401a603
William Blackwell <williamblackwell@Williams-MacBook-Air.local>
2017-02-22 18:17:39 -0600

    new

--- main/settings.py
+++ main/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = [
     os.path.join(BASE_DIR,"static"),
-    '/djregistration/apps/main/static/',
+    '/djregistration/apps/main/static/css',
 ]
